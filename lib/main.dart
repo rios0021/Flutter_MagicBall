de3dbf165a09543082e1d9dba8fdceca629af5b5
Eduardo Rios Corlay <rios0021@algonquinlive.com>
2020-05-07 22:34:36 -0400

    App completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,54 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: BallPage(),
       ),
     );
+
+
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
+      ),
+      body: Ball(),
+    );
+  }
+}
+
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: FlatButton(
+          child: Image.asset('images/ball$ballNumber.png'),
+          onPressed: (){
+            setState(() {
+              print('I got clicked');
+              ballNumber = Random().nextInt(5)+1;
+              print(ballNumber);
+            });
+          },
+        ),
+      ),
+    );
+  }
+}
